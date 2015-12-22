25098ed4ba246bbf040fa579f15343f0e0546a11
kmalhotra30 <kmalhotra30@gmail.com>
2015-12-22 23:34:57 +0530

    Top coder december hackathon 2015 files

---
+++ module.js
@@ -0,0 +1,12 @@
+var app=angular.module('we',[]);
+
+app.controller('mainc',function($scope,$http){
+     $scope.y=encodeURIComponent(item);
+     $http.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&order=viewCount&q="+$scope.y +"&type=video&key=AIzaSyBo8ZGrVp0mXWgMQP0DT8seAEQTCtuOvFI")
+      .success(function(data){
+        $scope.w=data;
+      })
+      .error(function(err){
+        return err;
+      });
+  });
---
+++ module2.js
@@ -0,0 +1,24 @@
+var app=angular.module('we',[]);
+
+app.controller('mainc',function($scope,$http){
+     $scope.y=encodeURIComponent(item);
+     $http.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&order=viewCount&q="+$scope.y +"&type=video&key=AIzaSyBo8ZGrVp0mXWgMQP0DT8seAEQTCtuOvFI")
+      .success(function(data){
+        $scope.w=data;
+        $scope.x=$scope.w.nextPageToken;
+                              $http.get("https://www.googleapis.com/youtube/v3/search?part=snippet&pageToken=" + $scope.x+ "&maxResults=50&order=viewCount&q="+$scope.y +"&type=video&key=AIzaSyBo8ZGrVp0mXWgMQP0DT8seAEQTCtuOvFI")
+              .success(function(data){
+                $scope.z=data;
+
+
+                })
+                .error(function(err){
+                  return err;
+                });
+
+
+      })
+      .error(function(err){
+        return err;
+      });
+  });
