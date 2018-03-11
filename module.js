2e3ddf265024aaa751d4ef854bb96250246ea037
kmalhotra30 <kmalhotra30@users.noreply.github.com>
2018-03-11 12:04:04 +0530

    Removed Key

--- module.js
+++ module.js
@@ -2,7 +2,7 @@ var app=angular.module('we',[]);
 
 app.controller('mainc',function($scope,$http){
      $scope.y=encodeURIComponent(item);
-     $http.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&order=viewCount&q="+$scope.y +"&type=video&key=AIzaSyBo8ZGrVp0mXWgMQP0DT8seAEQTCtuOvFI")
+     $http.get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&order=viewCount&q="+$scope.y +"&type=video&key=API_KEY")
       .success(function(data){
         $scope.w=data;
       })
